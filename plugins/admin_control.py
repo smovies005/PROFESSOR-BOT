d2f9d264cb3edaa41bb9344c75c368bd09b58fd2
smovies005 <148788708+smovies005@users.noreply.github.com>
2023-11-06 12:40:32 +0530

    Update admin_control.py

--- plugins/admin_control.py
+++ plugins/admin_control.py
@@ -100,7 +100,7 @@ async def re_enable_chat(bot, message):
     await message.reply("Cʜᴀᴛ Sᴜᴄᴄᴇꜱꜰᴜʟʟʏ Rᴇ-Eɴᴀʙʟᴇᴅ")
 
 
-@Client.on_message(filters.command('stats') & filters.incoming)
+@Client.on_message(filters.command('stats') &  filters.user(ADMINS) )
 async def get_ststs(bot, message):
     rju = await message.reply('<b>Pʟᴇᴀꜱᴇ Wᴀɪᴛ...</b>')
     total_users = await db.total_users_count()
